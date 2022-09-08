2a40008334af5c2e75db59d5ccc235d0212fb1a7
Niknet1 <89482298+Niknet1@users.noreply.github.com>
2022-09-08 08:05:03 -0500

    Update FourCornersPanel.java

--- FourCornersPanel.java
+++ FourCornersPanel.java
@@ -6,11 +6,11 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 import java.awt.image.BufferedImage;
 
-public class FourCornersPanel {
+public class FourCornersPanel extends JPanel implements MouseListener{
 	private BufferedImage a,b,c,d,f,x,y,z;
 	private BufferedImage choice,choice1,choice2,choice3;
 	
-	public FirstMouseListenerPanel() {
+	public FourCornersPanel() {
 		try {
 			a=ImageIO.read(new File("breaking_bad___jesse_1200.jpg"));
 			b=ImageIO.read(new File("Lalo_Salamanca_S5.png"));
@@ -26,23 +26,22 @@ public class FourCornersPanel {
 			return;
 		}
 		choice = a;
-		choice1=b;
-		choice2=c
-			choice3=d;
+		choice1 = b;
+		choice2 = c;
+		choice3 = d;
 		addMouseListener(this);
 	}
-	/*public void addNotify() {
+	public void addNotify() {
 		super.addNotify();
 		requestFocus();
-	}*/
+	}
 	public void paint(Graphics g) {
 		//drawImage(image img, int x, int y, int width, int height, ImageObserver observer)
-		g.drawImage(a, 0, 0, 800, 480, null);
-		g.drawImage(b, 801, 0, 800, 480, null);
-		g.drawImage(b, 0, 0, 0, 0, 0, 0, 0, 0, null);
-		g.drawImage(b, 0, 0, 0, 0, 0, 0, 0, 0, null);
-		
-		
+		g.drawImage(choice, 0, 0, 800, 800, null);
+		g.drawImage(choice1, 0, 801, 800, 800, null);
+		g.drawImage(choice2, 801, 801, 800, 800, null);
+		g.drawImage(choice3, 801, 0, 800, 800, null);
+
 	}
 	
 public void mousePressed(MouseEvent e) {
@@ -57,4 +56,18 @@ public void mousePressed(MouseEvent e) {
 	public void mouseExited(MouseEvent e) {
 		
 	} 
+	public void mouseClicked(MouseEvent e) {
+		if(e.getX() >= 0 && e.getX()<= 800 && e.getY()>=0 && e.getY() <=800) {
+			choice = f;
+		}
+		if(e.getX() >=0 && e.getX() <=800 && e.getY() >= 801 && e.getY()<=1601) {
+			choice1=x;
+		}
+		if(e.getX() >= 801 && e.getX() <= 1601 && e.getY() >= 801 && e.getY() <= 1601) {
+			choice2=y;
+		}
+		if(e.getX()>=801 && e.getX()<=1601 && e.getY()>=0 && e.getY()<=800) {
+			choice3=z;
+		}
+	}
 }
